eafb7e9d771b9ea01896a3d926a7b4550c37d5f7
Priyanka1234512345 <pandey.priyanka3898@gmail.com>
2021-02-01 15:22:47 +0530

    asynctask

---
+++ Desktop/asyctask/index.js
@@ -0,0 +1,35 @@
+var express = require("express");
+var async = require("async");
+var q = async.queue(function(task, callback) {
+    console.log('Hello ' + task.name);
+    callback();
+  }, 2);
+  
+  // assign a callback
+  q.drain = function() {
+    console.log('All items have been processed');
+  };
+  
+  // add some items to the queue
+  q.push({name: 'foo'}, function(err) {
+    console.log('Finished processing foo');
+  });
+  
+  q.push({name: 'bar'}, function (err) {
+    console.log('Finished processing bar');
+  });
+  
+  // add some items to the queue (batch-wise)
+  q.push([{name: 'baz'},{name: 'bay'},{name: 'bax'}], function(err) {
+    console.log('Finished processing item');
+  });
+
+  // add some items to the front of the queue
+  q.unshift({name: 'bar'}, function (err) {
+    console.log('Finished processing bar');
+  });
+
+
+
+
+
